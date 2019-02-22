daab9a0f2e34eb03ce21f134b6139b8bd8d4d45e
Ashok Adhikari <ashok@Ashoks-MacBook-Pro.local>
2019-02-22 16:49:15 -0600

    Did TDD practice with Java

---
+++ src/main/java/guru/springframework/Money.java
@@ -0,0 +1,56 @@
+package guru.springframework;
+
+public  class Money implements Expression{
+
+    protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    protected String currency() {
+        return currency;
+    }
+
+    public static Money dollar(int amount){
+        return new Money(amount, "USD");
+    }
+
+    public static Money franc(int amount){
+        return new Money(amount, "CHF");
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Money money = (Money) obj;
+
+        return amount == money.amount && this.currency() == money.currency();
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+
+    @Override
+    public Money reduce(Bank bank, String to){
+        //return this;
+        //int rate = (currency.equals("CHF") && to.equals("USD")) ? 2 : 1;
+        return new Money(amount / bank.rate(this.currency, to), to);
+    }
+
+    @Override
+    public Expression plus(Expression addend){
+        return new Sum(this, addend);
+    }
+
+    @Override
+    public Expression times(int multiplier){
+        return new Money(amount * multiplier, this.currency);
+    }
+}
